8849570f2c5d6c10b08dda1d9a9ad6fc43efc482
gauravsood150 <gsood0162@gmail.com>
2017-02-26 00:42:35 +0530

    Update Rod-cutting.cpp

--- Rod-cutting.cpp
+++ Rod-cutting.cpp
@@ -1,3 +1,9 @@
+/*Given a rod of length n inches and an array of prices that contains prices of
+
+all pieces of size smaller than n. Determine the maximum value obtainable
+
+by cutting up the rod and selling the pieces.*/
+
 #include <iostream>
 #include <limits.h>	 //INT_MIN
 #include <algorithm> //std::max
