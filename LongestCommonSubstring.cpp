d9cad260eee090b8b53c1dda53598ca9a6880e5d
gauravsood150 <gsood0162@gmail.com>
2017-02-26 16:50:52 +0530

    Add files via upload

---
+++ LongestCommonSubstring.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <limits.h>
+
+int LCS(std::string str1, std::string str2, int m, int n)
+{
+	int table[m+1][n+1];
+	int maxi=INT_MIN;
+	for(int i=0; i<=m; i++)
+		for(int j=0; j<=n; j++)
+		{
+			if(i==0 || j==0) table[i][j]=0;
+
+			else if(str1[i-1]==str2[j-1])
+			{	
+				table[i][j]=1+table[i-1][j-1];
+				if(maxi < table[i][j])
+					maxi=table[i][j];
+			}
+			else
+				table[i][j]=0;
+		}
+	return maxi;
+}		
+
+int main()
+{
+	std::string str1;
+	std::string str2;
+	std::cin>>str1>>str2;
+	int m=str1.size();
+	int n=str2.size();
+	std::cout<<LCS(str1,str2,m,n)<<std::endl;
+	return 0;
+}
