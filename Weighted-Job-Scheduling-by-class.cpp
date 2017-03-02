b5f5d2e9e3f6db642654f972ceae3d676e57eafb
gauravsood150 <gsood0162@gmail.com>
2017-03-02 17:34:35 +0530

    Update and rename WJS_by_class.cpp to Weighted-Job-Scheduling-by-class.cpp

--- Weighted-Job-Scheduling-by-class.cpp
+++ Weighted-Job-Scheduling-by-class.cpp
@@ -1,3 +1,10 @@
+/*Given N jobs where every job is represented by following three elements of it.
+
+Start Time
+Finish Time
+Profit or Value Associated
+Find the maximum profit subset of jobs such that no two jobs in the subset overlap.*/
+
 #include <iostream>
 #include <algorithm>
 using namespace std;
